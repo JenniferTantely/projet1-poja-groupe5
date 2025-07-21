288283f0c09e546dc5ef6f36a4186cbf3bd0ace3
JenniferTantely <hei.jennifer.2@gmail.com>
2025-07-21 16:34:41 +0300

    feat: change EnvService format

--- src/main/java/com/example/demo/service/EnvService.java
+++ src/main/java/com/example/demo/service/EnvService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 @Getter
 @Service
 public class EnvService {
+
   private final String secretKey;
 
   public EnvService(@Value("${secret.key}") String secretKey) {
