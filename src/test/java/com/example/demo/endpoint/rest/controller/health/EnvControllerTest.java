baa93a5bbb02b5348a7fc64759c404e88c3d40fa
JenniferTantely <hei.jennifer.2@gmail.com>
2025-07-21 15:09:18 +0300

    feat: add EnvControllerTest.java

---
+++ src/test/java/com/example/demo/endpoint/rest/controller/health/EnvControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.endpoint.rest.controller.health;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.demo.conf.FacadeIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+class EnvControllerTest extends FacadeIT {
+
+  @Autowired private TestRestTemplate restTemplate;
+
+  @Value("${secret.key}")
+  private String secretKey;
+
+  @Test
+  void can_read_secret_key() {
+    String response = restTemplate.getForObject("/env/secret-key", String.class);
+
+    assertThat(secretKey)
+        .withFailMessage("La variable d'environnement secret.key n'est pas définie.")
+        .isNotNull();
+
+    assertThat(response).isEqualTo(secretKey);
+  }
+}
