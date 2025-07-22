3bf3bbd7a798caf6e6948db6bce002a304997b2b
JenniferTantely <hei.jennifer.2@gmail.com>
2025-07-22 14:01:34 +0300

    feat: add Logger to the EnvControllerTest.java

--- src/test/java/com/example/demo/endpoint/rest/controller/health/EnvControllerTest.java
+++ src/test/java/com/example/demo/endpoint/rest/controller/health/EnvControllerTest.java
@@ -4,11 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.demo.conf.FacadeIT;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 class EnvControllerTest extends FacadeIT {
+  private static final Logger log = LoggerFactory.getLogger(EnvControllerTest.class);
+
   @Autowired private TestRestTemplate restTemplate;
 
   @Value("${secret.key}")
@@ -16,6 +20,8 @@ class EnvControllerTest extends FacadeIT {
 
   @Test
   void can_read_secret_key() {
+    log.info("Lancement du test can_read_secret_key");
+
     String response = restTemplate.getForObject("/env/secret-key", String.class);
 
     assertThat(secretKey)
