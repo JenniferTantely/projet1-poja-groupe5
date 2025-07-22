c596270362852b4d287a5d004115fc74a61b0489
JenniferTantely <hei.jennifer.2@gmail.com>
2025-07-22 14:48:20 +0300

    fixup

--- src/test/java/com/example/demo/service/EnvServiceTest.java
+++ src/test/java/com/example/demo/service/EnvServiceTest.java
@@ -7,14 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 class EnvServiceTest extends FacadeIT {
-    @Autowired EnvService subject;
+  @Autowired EnvService subject;
 
-    @Value("${secret.key}")
-    String expected;
+  @Value("${secret.key}")
+  String expected;
 
-    @Test
-    void get_env_ok() {
-        var actual = subject.getSecretKey();
-        Assertions.assertEquals(expected, actual);
-    }
+  @Test
+  void get_env_ok() {
+    var actual = subject.getSecretKey();
+    Assertions.assertEquals(expected, actual);
+  }
 }
