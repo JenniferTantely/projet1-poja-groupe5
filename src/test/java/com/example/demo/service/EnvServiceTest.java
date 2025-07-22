fea5910bf62089ed404726b548f956f317250548
JenniferTantely <hei.jennifer.2@gmail.com>
2025-07-22 14:45:24 +0300

    feat: add EnvService

---
+++ src/test/java/com/example/demo/service/EnvServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.conf.FacadeIT;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+class EnvServiceTest extends FacadeIT {
+    @Autowired EnvService subject;
+
+    @Value("${secret.key}")
+    String expected;
+
+    @Test
+    void get_env_ok() {
+        var actual = subject.getSecretKey();
+        Assertions.assertEquals(expected, actual);
+    }
+}
